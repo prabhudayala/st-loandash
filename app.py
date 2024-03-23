8dd6b933f556c97ba31b72c67865961b51b52c0e
guptaxvivek <gupta.vi2001@gmail.com>
2024-03-24 01:36:38 +0530

    base login page

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def main():
     st.markdown("""
         <style>
             .title {
-                color: #f63366;
+                color: #820263;
                 text-align: center;
                 font-size: 2.5em;
                 margin-bottom: 1em;
@@ -27,19 +27,21 @@ def main():
         </style>
     """, unsafe_allow_html=True)
     
-    st.markdown("<h1 class='title'>Login Page</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 class='title'>Login</h1>", unsafe_allow_html=True)
     
     # Create input fields for username and password
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
-    
+    col1, col2, col3, col4 = st.columns(4)
     # Create a styled login button
-    if st.button("Login", key="login-btn", type="primary"):
-        # if username == "admin" and password == "password":
-        st.success("Logged in as: {}".format(username))
-            # Redirect to the dashboard or another page
-        # else:
-        #     st.error("Invalid username or password")
+    with col1:
+        login_btn = st.button("Login", key="login-btn", type="primary", use_container_width=True)
+    if login_btn:
+        if username  and password :
+            st.success("Logged in as: {}".format(username))
+        # Redirect to the dashboard or another page
+        else:
+            st.error("Please input valid username and password")
 
 if __name__ == "__main__":
     main()

2bf20d0601eafd582d28e752b63e21ef2abda46e
Vivek <gupta.vi2001@gmail.com>
2024-04-03 11:49:34 +0530

    remove 3rd graph

--- pages/chargeoff_dash.py
+++ pages/chargeoff_dash.py
@@ -32,14 +32,14 @@ charge_offs_by_date.columns = ['Charge Off Month Year', 'Charge Off Amount']
 fig2 = px.bar(charge_offs_by_date, x='Charge Off Month Year', y='Charge Off Amount', title='Charge-offs by Date')
 
 
-fig3 = px.bar(data, x='Charge Off Month', y='Cur Balance', title='Charge-offs by Date', barmode='group', color='Dealer')
+# fig3 = px.bar(data, x='Charge Off Month', y='Cur Balance', title='Charge-offs by Date', barmode='group', color='Dealer')
 
 
 
 col1, col2 = st.columns(2)
 col1.plotly_chart(fig1, use_container_width=True)
 col2.plotly_chart(fig2, use_container_width=True)
-st.plotly_chart(fig3, use_container_width=True)
+# st.plotly_chart(fig3, use_container_width=True)
 c = st.columns([9,1]) 
 back = c[0].button('Back')
 next = c[1].button('Next')
